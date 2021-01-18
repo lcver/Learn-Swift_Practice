4cc4d50e3488a89452293ac0ea052904fa2fdf28
lcver <crackover1092@gmail.com>
2021-01-19 05:52:35 +0700

    [add] Set stored data

--- BirthdayNote/ViewController.swift
+++ BirthdayNote/ViewController.swift
@@ -17,9 +17,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        // Casting - as? vs. as!
+        
+        if let myName = storedName as? String,
+           let myBirthday = storedBirthday as? String{
+            nameLabel.text = "Name : \(myName)"
+            birthdayLabel.text = "Birthday : \(myBirthday)"
+        }
     }
 
     @IBAction func saveClick(_ sender: Any) {
+        
+        UserDefaults.standard.set(nameTextField.text, forKey: "name")
+        UserDefaults.standard.set(birthdayTextField.text, forKey: "birthday")
+        
         nameLabel.text = "Name : \(nameTextField.text!)"
         birthdayLabel.text = "Birthday : \(birthdayTextField.text!)"
     }
