6fc8a276d2c33228f867b5969592d03a46fc7148
lcver <crackover1092@gmail.com>
2021-01-19 05:28:32 +0700

    [create] layout view

--- BirthdayNote/ViewController.swift
+++ BirthdayNote/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func saveClick(_ sender: Any) {
+        nameLabel.text = "Name : \(nameTextField.text!)"
+        birthdayLabel.text = "Birthday : \(birthdayTextField.text!)"
+    }
+    
 }
 
