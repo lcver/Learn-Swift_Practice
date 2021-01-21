8863dff6d1a5883e6f93e496d62bc4e003d67b19
lcver <crackover1092@gmail.com>
2021-01-22 04:06:56 +0700

    [finish] Gesture Recognize App

--- GestureRecognizeApp/GestureRecognizeApp/ViewController.swift
+++ GestureRecognizeApp/GestureRecognizeApp/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        image.isUserInteractionEnabled = true
+        
+        // Target is a viewcontroller
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePict))
+        
+        image.addGestureRecognizer(gestureRecognizer)
     }
-
+    
+    @objc func changePict() {
+        
+        if label.text == "Pandem Body Kit" {
+            image.image = UIImage(named: "libertywalkBodyKit")
+            label.text = "Liberty Walk Bodykit"
+        } else if label.text == "Liberty Walk Bodykit" {
+            image.image = UIImage(named: "pandemBodyKit")
+            label.text = "Pandem Body Kit"
+        }
+    }
 
 }
 
