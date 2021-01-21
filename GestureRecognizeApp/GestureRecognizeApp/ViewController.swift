64d43f2f2de32661647729ea9f441bd12430db0c
lcver <crackover1092@gmail.com>
2021-01-22 03:21:54 +0700

    [add] Gesture Recognize App

---
+++ GestureRecognizeApp/GestureRecognizeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestureRecognizeApp
+//
+//  Created by Yapindo on 22/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
