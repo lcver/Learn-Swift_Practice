1067666d747fbb9bce0c77608789cdd8779e7220
lcver <crackover1092@gmail.com>
2021-01-22 22:40:18 +0700

    [add] Creating button

--- ObjectsWithCode/ObjectsWithCode/ViewController.swift
+++ ObjectsWithCode/ObjectsWithCode/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var myLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,7 +19,6 @@ class ViewController: UIViewController {
         let height = view.frame.size.height // current height
         
         // Create label
-        let myLabel = UILabel()
         myLabel.text = "test label"
         myLabel.textAlignment = .center
         /** CGrect Size
@@ -29,7 +30,19 @@ class ViewController: UIViewController {
         myLabel.frame = CGRect(x: (width - width * 0.8)/2, y: height * 0.5, width: width * 0.8, height: 50)
         view.addSubview(myLabel)
         
+        let myButton = UIButton()
+        myButton.setTitle("my Button", for: UIControl.State.normal)
+        myButton.setTitleColor(UIColor.blue, for: UIControl.State.normal)
+        myButton.frame = CGRect(x: width * 0.5 - 100, y: height * 0.7, width: 200 , height: 100)
+        view.addSubview(myButton)
+        
+        myButton.addTarget(self, action: #selector(ViewController.myFunction), for: UIControl.Event.touchUpInside)
         print("running")
+    }
+    
+    @objc func myFunction() {
+        myLabel.text = "button Tapped"
+        print("Tapped")
     }
 
 
