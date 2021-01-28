3025043c0c7d1dcd9a885551b709c85ab125b920
lcver <crackover1092@gmail.com>
2021-01-29 02:55:29 +0700

    [add] preparing data

--- LandmarkBook/LandmarkBook/ViewController.swift
+++ LandmarkBook/LandmarkBook/ViewController.swift
@@ -16,6 +16,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
+        
+        var carsName = [String]()
+        carsName.append("gtr-pandem")
+        carsName.append("gtr-libertywalk")
+        carsName.append("chevrolet")
+        carsName.append("mazda-rx7-red")
+        
+        var carsImage = [UIImage]()
+        carsImage.append(UIImage(named: "gtr-pandem")!)
+        carsImage.append(UIImage(named: "gtr-libertywalk")!)
+        carsImage.append(UIImage(named: "chevrolet")!)
+        carsImage.append(UIImage(named: "mazda-rx7-red")!)
+        
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
