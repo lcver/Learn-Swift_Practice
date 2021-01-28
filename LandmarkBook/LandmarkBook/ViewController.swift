f70e74ab2fdafe9c54117d314876bde56c44cccc
lcver <crackover1092@gmail.com>
2021-01-28 13:32:43 +0700

    [init] Initial commit Landmark Boox

---
+++ LandmarkBook/LandmarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Yapindo on 28/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
